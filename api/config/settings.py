35589866c95527b2bca8bfea14f91a94f35158d9
BlazejBielski <Blazej Bielski>
2023-05-03 22:34:59 +0200

    postgres setup according to 12factory app rules

--- api/config/settings.py
+++ api/config/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,12 +76,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASE_URL = os.environ.get("DB_CONNECTION_STRING")
+DATABASES = {"default": dj_database_url.config(default=DB_CONNECTION_STRING)}
 
 
 # Password validation
