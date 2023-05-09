0c39e42c4b501b5c96b5cc674c66171cd0bbe951
BlazejBielski <Blazej Bielski>
2023-05-09 21:14:02 +0200

    resolve conflicts

--- api/config/settings.py
+++ api/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "users.apps.UsersConfig",
     "shortener.apps.ShortenerConfig",
 ]
--- api/shortener/views.py
+++ api/shortener/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from django.views.generic import RedirectView
+from rest_framework import generics, status
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
+from . import models, serializers
+from .serializers import URLSerializer
+
+
+class CreateShortURLApiView(generics.CreateAPIView):
+    queryset = models.URL.objects.all()
+    serializer_class = serializers.OriginalURLSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        url = serializer.save()
+
+        short_url = URLSerializer(instance=url, context={"request": request})
+
+        return Response(data=short_url.data, status=status.HTTP_201_CREATED)
+
+
+class ShortURLRedirectView(RedirectView):
+    def get_redirect_url(self, *args: Any, short_url: str, **kwargs: Any) -> str | None:
+        url = get_object_or_404(models.URL, short_url=short_url)
+
+        return url.url
