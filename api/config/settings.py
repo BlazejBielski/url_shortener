d14f66b5d2cf1af5d8512c5c69036ffd9eaf8f41
BlazejBielski <Blazej Bielski>
2023-05-10 19:16:42 +0200

    update debug on false, setup correct allowed hosts

--- api/config/settings.py
+++ api/config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "secret"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = '127.0.0.1'
+ALLOWED_HOSTS = ['desolate-anchorage-51220.herokuapp.com']
 
 # Application definition
 
